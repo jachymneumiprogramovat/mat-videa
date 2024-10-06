10ebffd6a7bcc31b2d11027e5e411eada955fff0
Jáchym L <jachym.lowenhoffer@gmail.com>
2024-10-06 12:15:06 +0200

    cambié todos los problemas que madam pekac encontro

--- vectors/vectors.py
+++ vectors/vectors.py
@@ -8,7 +8,7 @@ PRAVA = constants.PRAVA
 
 class Vectors(Scene):
     def construct(self):
-        self.next_section(skip_animations=False)
+        self.next_section(skip_animations=True)
         self.section1()
         self.next_section(skip_animations=False)
         self.section2()
@@ -33,7 +33,8 @@ class Vectors(Scene):
                 "stroke_opacity": 0.5   # Opacity of the grid lines
             },
             axis_config={"color": WHITE, "stroke_width": 2},  # Customize axis appearance
-            faded_line_ratio=2  # To make the lines less dominant
+            faded_line_ratio=2,  # To make the lines less dominant
+            z_index=-1
         )
 
         # Shift grid and axes to the left to make space for text on the right
@@ -48,8 +49,9 @@ class Vectors(Scene):
         axes.shift(RIGHT * 2)
 
         # Add some text to the left side
-        def1 = Tex(r"Vektor je dvojice čísel\\nakresleme si ho tedy\\ jako bod v rovině.")
+        def1 = Tex(r"Vektor je šipka\\ z počátku souřadnic\\ do nějakého bodu.")
         def1.move_to(LEFT*3.5+UP*2.5)  # Position the text on the left side
+
 
         # Add everything to the scene
 
@@ -57,33 +59,40 @@ class Vectors(Scene):
         self.play(Write(def1))
         self.wait(2)
 
-        dot = Dot(axes.c2p(2,3))
+        vector = Vector(axes.c2p(2,3),color=RED_D,z_index=2)
+        self.play(FadeIn(vector))
+        self.wait(2)
+
+        dot = Dot(axes.c2p(2,3),z_index=3)
         dot_label = Tex(r"(2,3)").next_to(dot,UP)
+
+        def2 = Tex(r"Protože začátek je pro\\ každý vektor stejný,\\ budeme si pamatovat\\ hlavně jeho konec."
+                   ).move_to(LEFT*LEVA+DOWN*DOLU)
+        self.play(Write(def2))
 
         self.play(FadeIn(dot),Write(dot_label))
         self.wait(2)
         
 
-        def2 = Tex(r"Do tohoto bodu udělejme \\ šipku z počátku souřadnic.\\Šipka tedy indikuje vektor.").move_to(LEFT*LEVA+DOWN*DOLU)
-        self.play(Write(def2))
-
-        vector = Vector(axes.c2p(2,3),color=RED_D)
-        self.play(FadeIn(vector))
-        self.wait(2)
-
-        znaceni = MathTex(r"(2,3)=\vec{v}").move_to(dot_label).shift(LEFT*0.3)
-        znaceni[0][6:8].set_color(RED_D)
-        self.play(TransformMatchingTex(dot_label,znaceni))
+        vec = Tex("Vektor značíme šipkou.").move_to(RIGHT*PRAVA+DOWN*DOLU)
+        
+        znaceni = MathTex("=",r"\vec{v}").move_to(dot_label).shift(RIGHT*0.5)
+        znaceni[1].set_color(RED_D)
+        self.play(Write(vec))
+        self.play(dot_label.animate.shift(LEFT*0.5),FadeIn(znaceni,shift=RIGHT*0.5))
+        self.wait()
+        self.play(Circumscribe(znaceni[1]))
         self.wait(2)
         
 
-        self.play(FadeOut(def1,def2,znaceni,dot))
+        self.play(FadeOut(def1,def2,znaceni,dot,dot_label,vec))
 
-        velikost = Tex(r"Co nás u vektorů bude\\ zajímat je například\\ jejich délka.").move_to(LEFT*LEVA+UP*NAHORU)
+        velikost = Tex(r"Co nás u vektorů bude\\ zajímat, je například\\ jejich Velikost."
+                       ).move_to(LEFT*LEVA+UP*NAHORU)
 
-        general = Vector(axes.c2p(4,4),color=RED_D)
+        general = Vector(axes.c2p(4,4),color=RED_D,z_index=2)
         self.play(Transform(vector,general))
-        obecne= MathTex(r"\vec{v}=(x,y)").next_to(general,UP).shift(RIGHT)
+        obecne= MathTex(r"\vec{v}=(v_x,v_y)").next_to(general,UP).shift(RIGHT*2)
         obecne[0][0:2].set_color(RED_D)
 
         self.play(Write(obecne))
@@ -91,35 +100,40 @@ class Vectors(Scene):
         self.play(Write(velikost))
         self.wait(2)
 
-        delka = Tex(r"Délku vektoru značíme $\|\vec{v}\|$\\ a z pytágorovi věty\\ spočítáme jako:").move_to(LEFT*LEVA+DOWN*DOLU)
+        delka = Tex(r"Velikost vektoru značíme $| \vec{v} |$\\ a z pytágorovi věty\\ spočítáme jako:"
+                    ).move_to(LEFT*LEVA+DOWN*DOLU)
         delka[0][23:25].set_color(RED_D)
-        vzorec = MathTex(r"\|\vec{v}\| = \sqrt{x^2 + y^2}").next_to(delka,RIGHT).shift(RIGHT*2)
+        vzorec = MathTex(r"| \vec{v} | = \sqrt{v_x^2 + v_y^2}").next_to(delka,RIGHT).shift(RIGHT*3)
         vzorec[0][1:3].set_color(RED_D)
 
-        vzorec[0][7:9].set_color(YELLOW)
-        vzorec[0][10:14].set_color(YELLOW)
+        vzorec[0][7:10].set_color(YELLOW)
+        vzorec[0][11:15].set_color(YELLOW)
+
+        v_box = SurroundingRectangle(
+            vzorec, color=WHITE, fill_color=BLACK, fill_opacity=1, z_index=0,buff=0.2
+        )
+
 
         self.play(Write(delka))
         self.wait(3)
-        self.play(Write(vzorec))
+        self.play(AnimationGroup(DrawBorderThenFill(v_box),Write(vzorec)))
         self.wait(3)
 
         x = DashedLine(axes.c2p(4,4),axes.c2p(4,0),color=YELLOW)
-        x_lab = MathTex(r"y",color=YELLOW).next_to(x.get_last_handle(),RIGHT).shift(UP)
-        y = DashedLine(axes.c2p(4,4),axes.c2p(0,4),color=YELLOW)
-        y_lab = MathTex(r"x",color=YELLOW).next_to(y.get_last_handle(),UP).shift(RIGHT)
+        x_lab = MathTex(r"v_y",color=YELLOW).next_to(x.get_last_handle(),RIGHT).shift(UP)
+        y = DashedLine(axes.c2p(4,0),axes.c2p(0,0),color=YELLOW,z_index=1)
+        y_lab = MathTex(r"v_x",color=YELLOW).next_to(y.get_last_handle(),DOWN).shift(RIGHT*3)
 
         self.play(Write(x),Write(y))
         self.play(Write(x_lab),Write(y_lab))
         self.wait()
-        self.play(Indicate(obecne[0][4]),Indicate(obecne[0][6]))
+        self.play(Indicate(obecne[0][4:6]),Indicate(obecne[0][7:9]))
         self.wait(5)
 
-        self.play(FadeOut(x,y,y_lab,x_lab,delka,velikost,vzorec,vector,obecne))
+        self.play(FadeOut(x,y,y_lab,x_lab,delka,velikost,vzorec,vector,obecne,axes,v_box))
 
         global scene
-        scene = VGroup(axes)
-        self.play(FadeOut(scene))
+        scene=axes
 
 
     def section2(self):
@@ -130,20 +144,32 @@ class Vectors(Scene):
         self.wait(1)
         self.play(FadeOut(intro))
 
+        axes.x_axis.numbers.set_opacity(0)
+        axes.y_axis.numbers.set_opacity(0)
+
         self.play(FadeIn(scene))
 
-        skalovani = Tex(r"Násobení vektoru číslem\\ neboli škálování je intuitivní:"
+        skalovani = Tex(r"Násobení vektoru číslem,\\ neboli škálování, je intuitivní:"
                         ).move_to(LEFT*LEVA+UP*NAHORU)
         gen = MathTex(r"\alpha", r"\vec{v}",r" = (\alpha ",r"v_x",r",\alpha",r"v_y",")"
-                        ).move_to(LEFT*LEVA+UP*1.5)
+                        ).move_to(LEFT*LEVA+UP*1)
         gen[1].set_color(RED_D)
         gen[3].set_color(RED_D)
         gen[5].set_color(RED_D)
+
+        gen[0].set_color(GOLD_C)
+        gen[2][2:].set_color(GOLD_C)
+        gen[4][1:].set_color(GOLD_C)
+
+        g_box = SurroundingRectangle(
+            gen, color=WHITE, fill_color=BLACK, fill_opacity=1, z_index=0,buff=0.2
+        )
 
         popis = Tex("Kde", r"~$v_x$~", r"značí $x$-ovou\\ složku vektoru",r"~$\vec{v}$~",r"a $\alpha \in \mathbb{R}$."
                     ).move_to(DOWN*DOLU+RIGHT*PRAVA)
         popis[1].set_color(RED_D)
         popis[3].set_color(RED_D)
+        popis[4][1].set_color(GOLD_C)
 
         c=ValueTracker(1)
         normal = Vector(axes.c2p(c.get_value()*2,c.get_value()*2),color=RED_D)
@@ -154,11 +180,11 @@ class Vectors(Scene):
         def nl_updater(m):
             if c.get_value()>0:
                 m.become(
-                    MathTex(f"{np.round(c.get_value(),decimals=2)}",r"\vec{v}").next_to(normal,RIGHT)
+                    MathTex(f"{c.get_value():.2f}",r"\vec{v}").next_to(normal,RIGHT)
                 )
             else:
                 m.become(
-                    MathTex(f"{np.round(c.get_value(),decimals=2)}",r"\vec{v}").next_to(normal,LEFT)
+                    MathTex(f"{c.get_value():.2f}",r"\vec{v}").next_to(normal,LEFT)
                 )
             m[1].set_color(RED_D)
 
@@ -166,18 +192,18 @@ class Vectors(Scene):
         nl.add_updater(nl_updater)
         
         self.play(Write(skalovani))
-        self.wait()
-        self.play(FadeIn(normal),FadeIn(nl))
+        self.wait(1)
+        self.play(FadeIn(normal),Write(nl))
         self.wait(2)
-        self.play(Write(gen))
+        self.play(AnimationGroup(DrawBorderThenFill(g_box),Write(gen)))
         self.wait(2)
         self.play(Write(popis))
         self.wait(6)
-        self.play(c.animate.set_value(2),run_time=3)
-        self.play(c.animate.set_value(-2),run_time=3)
+        self.play(c.animate.set_value(2),run_time=3.5)
+        self.play(c.animate.set_value(-2),run_time=3.5)
         self.wait()
 
-        self.play(FadeOut(nl,normal,skalovani,gen,popis))
+        self.play(FadeOut(nl,normal,skalovani,gen,popis,g_box))
 
 
         self.next_section(skip_animations=False)
@@ -190,7 +216,8 @@ class Vectors(Scene):
         scitani[0][33:35].set_color(GREEN_D)
         
         vec1 = Vector(axes.c2p(3,5),color=RED_D)
-        v_lab = MathTex(r"\vec{v}",color=RED_D).next_to(vec1,LEFT).shift(RIGHT)
+        v_lab = MathTex(r"\vec{v}",color=RED_D,z_index=1
+                        ).next_to(vec1,LEFT).shift(RIGHT)
         vec2 = Vector(axes.c2p(1,-3),color=GREEN_D)
         u_lab = MathTex(r"\vec{u}",color=GREEN_D).next_to(vec2,RIGHT)
 
@@ -251,7 +278,7 @@ class Vectors(Scene):
         self.play(FadeIn(w,w_lab))
         self.wait(3)
         self.play(AnimationGroup(DrawBorderThenFill(w_box),Write(vzorec)))
-        self.wait(2)
+        self.wait(4)
 
         self.play(FadeOut(w,w_lab,vzorec,scitani,w_box,))
 
@@ -264,8 +291,8 @@ class Vectors(Scene):
 
         self.play(Transform(vec1,Vector(axes.c2p(2,2),color=RED_D)))
 
-        odcitani = Tex("Rozdíl",r"~$\vec{v}~$","a",r"~$\vec{u}$~",r"\\je součet prvního\\ odčítance s inverzem\\ toho druhého"
-                       ).move_to(LEFT*(LEVA+0.3)+UP*NAHORU)
+        odcitani = Tex("Rozdíl",r"~$\vec{v}~$","a",r"~$\vec{u}$~",r"\\je součet menšence s mínus\\ jedna násobkem menšitelem"
+                       ).move_to(LEFT*(LEVA+0.3)+UP*(NAHORU+0.2))
         odcitani[1].set_color(RED_D)
         odcitani[3].set_color(GREEN_D)
 
@@ -275,18 +302,18 @@ class Vectors(Scene):
         mu[1].set_color(GREEN_D)
 
         self.play(Write(odcitani))
-        self.wait(3)
+        self.wait(4)
         self.play(AnimationGroup(
-            Transform(vec2,Vector(axes.c2p(-1,2),color=GREEN_D)),
+            Transform(vec2,Vector(axes.c2p(-2,1),color=GREEN_D)),
             u_lab.animate.become(mu),
             ),lag_ratio=0.5)
         self.wait()
         self.play(Flash(u_lab[0]))
-        self.wait(2)
-        self.play(vec1.animate.move_to(axes.c2p(0,3)))
+        self.wait(3)
+        self.play(vec1.animate.move_to(axes.c2p(-1,2)))
         self.wait(3)
 
-        w2=Vector(axes.c2p(1,4),color=BLUE_D)
+        w2=Vector(axes.c2p(0,3),color=BLUE_D)
         w_l = MathTex(r"\vec{w}","=",r"\vec{v}","+ (-",r"\vec{u}",")",z_index=2
                       ).next_to(w2,RIGHT*0.1)
         w_l[0].set_color(BLUE_D)
@@ -294,7 +321,7 @@ class Vectors(Scene):
         w_l[4].set_color(GREEN_D)
 
         self.play(FadeIn(w2,w_l))
-        self.wait(2)
+        self.wait(3)
 
         aaaaa= Tex("Obecný vzorec je tedy:").move_to(DOWN*DOLU+LEFT*LEVA)
         self.play(Write(aaaaa))
@@ -314,7 +341,4 @@ class Vectors(Scene):
             vzorec, color=WHITE, fill_color=BLACK, fill_opacity=1, z_index=0,buff=0.2
         )
         self.play(AnimationGroup(DrawBorderThenFill(v_box),Write(vzor)))
-        self.wait(4)
-
-
-
+        self.wait(5)
